f7a2348f1125163fb8c46e5fefd4c9042fdbdcb4
Ravi Shekhar <ravishekhar@users.noreply.github.com>
2024-03-05 07:21:03 -0600

    first commit

---
+++ static/index.js
@@ -0,0 +1,78 @@
+import { loadScript } from "https://unpkg.com/@paypal/paypal-js@4.2.1/dist/esm/paypal-js.js";
+import { getOptionsFromQueryString, setPerformanceMark } from "./utils.js";
+
+const sdkScriptDefaultOptions = {
+    "client-id": "test",
+    cachebust: "calzone",
+};
+
+const sdkScriptOptionsFromQueryString = getOptionsFromQueryString();
+const sdkScriptOptions = Object.keys(sdkScriptOptionsFromQueryString).length
+    ? sdkScriptOptionsFromQueryString
+    : sdkScriptDefaultOptions;
+
+const buttonOptions = {
+    onError(err) {
+        console.error("buttons onError() callback", err);
+    },
+
+    createOrder(data, actions) {
+        return actions.order
+            .create({
+                purchase_units: [
+                    {
+                        amount: {
+                            value: 2.0,
+                        },
+                    },
+                ],
+            })
+            .then((orderID) => {
+                console.log({ orderID });
+                return orderID;
+            });
+    },
+
+    onApprove(data, actions) {
+        const outputElement = document.querySelector("#output");
+
+        return actions.order
+            .capture()
+            .then((orderDetails) => {
+                console.log({ orderDetails });
+                outputElement.innerHTML =
+                    "<h3>Thank you for your payment!</h3>";
+            })
+            .catch((err) => {
+                console.error(err);
+                outputElement.innerHTML = `<h3>The capture failed. \n ${err.toString()}</h3>`;
+            });
+    },
+};
+
+loadScript(sdkScriptOptions)
+    .then((paypal) => {
+        console.log("successfully loaded the JS SDK script");
+        setPerformanceMark("sdk-script-load");
+
+        let buttonsInstance;
+
+        try {
+            buttonsInstance = paypal.Buttons(buttonOptions);
+        } catch (err) {
+            console.error("failed to create the buttons component", err);
+        }
+
+        buttonsInstance
+            .render("#buttons-container")
+            .then(() => {
+                console.log("successfully rendered the paypal buttons");
+                setPerformanceMark("sdk-buttons-render");
+            })
+            .catch((err) => {
+                console.error("failed to render paypal buttons", err);
+            });
+    })
+    .catch((err) => {
+        console.error("failed to load the JS SDK script", err);
+    });
---
+++ static/utils.js
@@ -0,0 +1,42 @@
+export function getOptionsFromQueryString() {
+    const allowedSDKQueryParams = [
+        "components",
+        "debug",
+        "client-id",
+        "merchant-id",
+        "locale",
+        "currency",
+        "intent",
+        "commit",
+        "vault",
+        "buyer-country",
+        "enable-funding",
+        "disable-funding",
+        "disable-card",
+        "integration-date",
+        "data-namespace",
+        "sdkBaseURL",
+        "cachebust",
+        "cdn-registry",
+        "version",
+        "data-popups-disabled",
+        "data-uid",
+    ];
+
+    const searchParams = new URLSearchParams(window.location.search);
+    const validOptions = {};
+
+    searchParams.forEach((value, key) => {
+        if (allowedSDKQueryParams.includes(key)) {
+            validOptions[key] = value;
+        }
+    });
+
+    return validOptions;
+}
+
+export function setPerformanceMark(name) {
+    if (window.performance && performance.mark) {
+        performance.mark(name);
+    }
+}
